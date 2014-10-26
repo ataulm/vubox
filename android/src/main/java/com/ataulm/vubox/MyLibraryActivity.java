4c6d3f7def4ec36426af8dbbebcba7350c027956
Ataul Munim <timberwo7ves@gmail.com>
2014-10-26 23:00:16 +0000

    Remove unrelated code

--- android/src/main/java/com/ataulm/vubox/MyLibraryActivity.java
+++ android/src/main/java/com/ataulm/vubox/MyLibraryActivity.java
@@ -2,10 +2,6 @@ package com.ataulm.vubox;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-
-import java.util.List;
 
 public class MyLibraryActivity extends Activity {
 
@@ -13,17 +9,6 @@ public class MyLibraryActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_library);
-
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.trending_shows_list);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        TrendingShowsAdapter adapter = TrendingShowsAdapter.newInstance();
-        recyclerView.setAdapter(adapter);
-
-        adapter.onUpdate(getTrendingShows());
-    }
-
-    private List<Show> getTrendingShows() {
-        return new ShowsProvider().fetchTrendingShows();
     }
 
 }

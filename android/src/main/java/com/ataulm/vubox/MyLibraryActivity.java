a4eecf7bfce3676143769735c184cec84b61c4ba
Ataul Munim <timberwo7ves@gmail.com>
2014-10-30 14:31:31 +0000

    Fix RecyclerView XML declaration

--- android/src/main/java/com/ataulm/vubox/MyLibraryActivity.java
+++ android/src/main/java/com/ataulm/vubox/MyLibraryActivity.java
@@ -2,6 +2,10 @@ package com.ataulm.vubox;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MyLibraryActivity extends Activity {
 
@@ -9,6 +13,10 @@ public class MyLibraryActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_library);
+
+        RecyclerView myLibrary = (RecyclerView) findViewById(R.id.mylib);
+        myLibrary.setLayoutManager(new LinearLayoutManager(this));
+        myLibrary.setAdapter(new MyLibraryAdapter());
     }
 
 }

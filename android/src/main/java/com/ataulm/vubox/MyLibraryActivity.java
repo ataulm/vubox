1cdb1cdff9c7dfa151723c4ae5a07376a2c5d726
Ataul Munim <timberwo7ves@gmail.com>
2014-10-31 17:10:19 +0000

    Add 3 column grid to MyLibrary

--- android/src/main/java/com/ataulm/vubox/MyLibraryActivity.java
+++ android/src/main/java/com/ataulm/vubox/MyLibraryActivity.java
@@ -2,19 +2,30 @@ package com.ataulm.vubox;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 public class MyLibraryActivity extends Activity {
+
+    private static final int COLUMN_COUNT = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_library);
-
         RecyclerView myLibrary = (RecyclerView) findViewById(R.id.mylib);
-        myLibrary.setLayoutManager(new LinearLayoutManager(this));
+        myLibrary.setLayoutManager(new GridLayoutManager(this, COLUMN_COUNT));
         myLibrary.setAdapter(new MyLibraryAdapter(getResources()));
+    }
+
+    private static class MyLibraryLayoutManager extends RecyclerView.LayoutManager {
+
+        @Override
+        public RecyclerView.LayoutParams generateDefaultLayoutParams() {
+            int wrapContent = RecyclerView.LayoutParams.WRAP_CONTENT;
+            return new RecyclerView.LayoutParams(wrapContent, wrapContent);
+        }
+
     }
 
 }
